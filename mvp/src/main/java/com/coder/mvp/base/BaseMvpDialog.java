074e2399bbd277fe5eb46c9ab23b4d9753313007
anjoiner <anjoiner@163.com>
2021-03-10 13:47:33 +0800

    新增base dialog

---
+++ mvp/src/main/java/com/coder/mvp/base/BaseMvpDialog.java
@@ -0,0 +1,173 @@
+package com.coder.mvp.base;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.viewbinding.ViewBinding;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public abstract class BaseMvpDialog <T extends ViewBinding> extends DialogFragment implements BaseVu{
+    protected View mRootView;
+    // 标志位 标志已经初始化完成。
+    protected boolean isPrepared;
+    //标志位 fragment是否可见
+    protected boolean isVisible;
+
+    protected Context mContext;
+    protected Activity mActivity;
+
+    private PresenterProviders mPresenterProviders;
+    private PresenterDispatch mPresenterDispatch;
+    private CompositeDisposable disposables;
+
+    protected T mViewBinding;
+
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+        mContext = activity;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog!=null){
+            initParams(dialog);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mRootView != null) {
+            ViewGroup parent = (ViewGroup) mRootView.getParent();
+            if (parent != null)
+                parent.removeView(mRootView);
+        } else {
+            mViewBinding = buildViewBinding(container);
+            if (mViewBinding != null && mViewBinding.getRoot() != null) {
+                mRootView = mViewBinding.getRoot();
+            } else {
+                mRootView = inflater.inflate(getLayoutId(), container, false);
+            }
+        }
+        onCreateStart();
+        return mRootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        disposables = new CompositeDisposable();
+        onPresenterCreate(savedInstanceState);
+        isPrepared = true;
+        init();
+        lazyLoad();
+    }
+
+    private T buildViewBinding(ViewGroup container) {
+        T binding = null;
+        Type superclass = getClass().getGenericSuperclass();
+        Class<?> aClass = (Class<?>) ((ParameterizedType) superclass).getActualTypeArguments()[0];
+        try {
+            Method method = aClass.getDeclaredMethod("inflate", LayoutInflater.class, ViewGroup.class, boolean.class);
+            binding = (T) method.invoke(null, getLayoutInflater(), container, false);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return binding;
+    }
+
+    private void onPresenterCreate(Bundle savedInstanceState) {
+
+        mPresenterProviders = PresenterProviders.inject(this);
+        mPresenterDispatch = new PresenterDispatch(mPresenterProviders, disposables);
+
+        mPresenterDispatch.attachView(getActivity(), this);
+        mPresenterDispatch.onCreatePresenter(savedInstanceState);
+    }
+
+    protected void initParams(Dialog dialog){
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0x00000000));
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(true);
+    }
+
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mPresenterDispatch.onSaveInstanceState(outState);
+    }
+
+    protected abstract int getLayoutId();
+
+    protected abstract void init();
+
+    protected abstract void onCreateStart();
+
+    protected void lazyLoadData() {}
+
+    protected void onVisible() {
+        lazyLoad();
+    }
+
+    protected void onInvisible() {}
+
+    protected PresenterProviders getPresenterProviders() {
+        return mPresenterProviders;
+    }
+
+    private void lazyLoad() {
+        if (!isPrepared || !isVisible) {
+            return;
+        }
+        lazyLoadData();
+        isPrepared = false;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getUserVisibleHint()) {
+            isVisible = true;
+            onVisible();
+        } else {
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mPresenterDispatch.detachView();
+        disposables.clear();
+    }
+
+    @Override
+    public void onDetach() {
+        this.mActivity = null;
+        super.onDetach();
+    }
+}
